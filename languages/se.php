c192de7e8eb1263f72145def6531755df1580eb1
Xpandedreality <lamin@xpandedreality.com>
2017-04-12 15:01:50 +0200

    Create se.php
    A Swedish translation.

---
+++ languages/se.php
@@ -0,0 +1,21 @@
+<?php
+
+l::set([
+    'uniform-filled-potty' => 'Fältet som ska vara tomt fylldes i. Om du inte är en spam-bot, försök igen och lämna fältet tomt.',
+
+    'uniform-calc-incorrect' => 'Lös matteproblemet.',
+
+    'uniform-email-subject' => 'Meddelande via webbformuläret',
+    'uniform-email-error' => 'Ett fel uppstod då formuläret skulle skickas:',
+    'uniform-email-copy' => 'Kopiera:',
+
+    'uniform-calc-plus' => 'plus',
+
+    'uniform-log-error' => 'Ett fel uppstod vid skrivning till loggfilen.',
+
+    'uniform-login-error' => 'Fel användarnamn eller lösenord.',
+
+    'uniform-webhook-error' => 'Ett fel uppstod vid anrop till webhook: ',
+
+    'uniform-email-select-error' => 'Ogiltig mottagare.',
+]);
